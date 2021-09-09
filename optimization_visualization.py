dddb803c9f37db1d99c29678d8412c37acb6bc65
Alex <alex@nowhere.com>
2021-09-09 16:25:20 -0700

    more tweaking of optimization_visualization

--- optimization_visualization.py
+++ optimization_visualization.py
@@ -7,13 +7,32 @@ from enum import Enum
 class OptVisCommands(Enum):
     InputPoll = 1
 
-def optimization_visualization(plot_rows, plot_cols, compute_fn, draw_fn, count_hint=None):
-    from optimization_visualization_session_store import optimization_visualization_session_store
+def inifinte_iterate(iter_fn):
+    def looper():
+        while True:
+            yield iter_fn()
+    return looper
+
+
+def optimization_visualization_subplots(plot_rows, plot_cols):
+    fig = plt.gcf()
+    fig.clf()
+    ax = fig.subplots(plot_rows, plot_cols)
+
+    # Force to top once (but not during running, intentional).
+    matplotlib.rcParams['figure.raise_window'] = True
+    plt.show()
+    matplotlib.rcParams['figure.raise_window'] = False
+
+    return fig, ax
+
+
+def optimization_visualization(fig, ax, compute_fn, draw_fn, count_hint=None):
+    from pyavlib.optimization_visualization_session_store import optimization_visualization_session_store
 
     # Remove previous run's listeners. TODO: reuse?
     # (Not removing at the end of run to allow interactivity after the run is done).
     print(f'{optimization_visualization_session_store=}')
-    fig = plt.gcf()
     for object in optimization_visualization_session_store:
         if isinstance(object, plt.Widget):
             object.disconnect_events()
@@ -22,16 +41,13 @@ def optimization_visualization(plot_rows, plot_cols, compute_fn, draw_fn, count_
     optimization_visualization_session_store.clear()
 
     keep_running = True
-    fig.clf()
-    ax = fig.subplots(plot_rows, plot_cols)
-
-    frame_count = count_hint if count_hint is not None else 0
+    frame_count = count_hint if count_hint is not None else 1
 
     fig.subplots_adjust(left=0.05, bottom=0.15, right=0.95, top=0.95)
     time_slider_ax = plt.axes([0.2, 0.05, 0.6, 0.03], facecolor='lightgoldenrodyellow')
     freq_slider = plt.Slider(
         ax=time_slider_ax,
-        label='Frame(0/12) Running...',
+        label='Starting up...',
         valmin=0,
         valmax=frame_count - 1,
         valinit=0,
@@ -69,19 +85,20 @@ def optimization_visualization(plot_rows, plot_cols, compute_fn, draw_fn, count_
             current_frame += 1
             if current_frame >= frame_count:
                 frame_count = current_frame + 1
-                freq_slider.label.set_text(f'Running ({frame_count})')
+                freq_slider.label.set_text(f'Running ({frame_count})...')
                 freq_slider.valmax = frame_count - 1
                 time_slider_ax.set_xlim(0, freq_slider.valmax)
             freq_slider.set_val(current_frame)
 
+
             if state is None:
-                draw_fn(ax)
+                draw_fn()
             else:
                 # https://stackoverflow.com/questions/1952464/in-python-how-do-i-determine-if-an-object-is-iterable
                 try:
-                    draw_fn(ax, *state)
+                    draw_fn(*state)
                 except TypeError:
-                    draw_fn(ax, state)
+                    draw_fn(state)
             plt.show()
             # thank you https://www.geeksforgeeks.org/how-to-update-a-plot-on-same-figure-during-the-loop/
             fig.canvas.draw()
@@ -92,9 +109,12 @@ def optimization_visualization(plot_rows, plot_cols, compute_fn, draw_fn, count_
 
 
 def main1():
-    x = np.linspace(-2.0, 2.0, num=256)
+    fig, ax = optimization_visualization_subplots(2, 2)
 
-    def draw(ax, mu, sigma=0.2):
+    x = np.linspace(-2.0, 2.0, num=256)
+    ax[0][0].plot(x, x * 0 + 1, 'k--')
+
+    def draw(mu, sigma=0.2):
         """
         :type fig: plt.Figure
         :type ax: list[list[plt.Axes]]
@@ -110,28 +130,29 @@ def main1():
 
     def compute():
         for mu in np.linspace(-1.0, 1.0, num=16):
-            for i in range(10):
-                time.sleep(0.1)  # Pretend there's a lot of compute here.
-                yield OptVisCommands.InputPoll # Cooperative multitasking :) Maybe not needed.
             yield mu
 
-    optimization_visualization(2, 2, compute, draw, count_hint=16)
+    optimization_visualization(fig, ax, compute, draw, count_hint=16)
 
 
 def main2():
+    fig, ax = optimization_visualization_subplots(2, 2)
+
     # We can also not return state (pass it via scope globals), but then we can't seek.
     mu = 0
 
     def compute():
         nonlocal mu
         for i in range(16):
-            time.sleep(0.1)  # Pretend there's a lot of compute here.
+            for i in range(10):
+                time.sleep(0.05)  # Pretend there's a lot of compute here.
+                yield OptVisCommands.InputPoll # Cooperative multitasking :) Maybe not needed.
             yield None
             mu += 0.1
 
     x = np.linspace(-2.0, 2.0, num=256)
 
-    def draw(ax):
+    def draw():
         """
         :type fig: plt.Figure
         :type ax: list[list[plt.Axes]]
@@ -146,7 +167,7 @@ def main2():
         plt.gca().set_aspect('equal', adjustable='datalim')
 
 
-    optimization_visualization(2, 2, compute, draw, count_hint=16)
+    optimization_visualization(fig, ax, compute, draw, count_hint=16)
 
 
 
@@ -154,3 +175,4 @@ def main2():
 
 if __name__ == "__main__":
     main1()
+    main2()
--- pltutil.py
+++ pltutil.py
@@ -1,10 +1,19 @@
 import matplotlib.pyplot as plt
 
+
 def plot_rect_wh(ax, x0, y0, w, h, col):
     plot_rect(ax, x0, y0, x0+w, y0+h, col)
 
+
 def plot_rect(ax, x0, y0, x1, y1, col):
     ax.plot([x0, x0, x1, x1, x0], [y0, y1, y1, y0, y0], col)
+
+
+def plt_sca_cla(ax, title=None):
+    plt.sca(ax)
+    ax.cla()
+    if title is not None:
+        plt.title(title)
 
 if __name__ == "__main__":
     print("start")
