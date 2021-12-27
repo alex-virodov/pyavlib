b1303d97118c22ededc9159cc3a14d325fd821fd
Alex <alex@nowhere.com>
2021-12-26 17:54:21 -0800

    minor change to template

--- template.py
+++ template.py
@@ -4,9 +4,9 @@ import cv2
 import numpy as np
 
 def main():
-    image = cv2.imread('image.png', cv2.IMREAD_GRAYSCALE)
-
     ax = plt_clf_subplots(3, 3)
+
+    image = cv2.imread('image.png', cv2.IMREAD_GRAYSCALE)
     ax[0].imshow(image).axes.set_title('image')
 
 if __name__ == '__main__':
