c572abf2838499e6f5a0ba78ab48eddaf3ff6efb
Alexandr Virodov <Alexandr.Virodov@uky.edu>
2022-08-28 14:39:52 -0400

    simple logistic regression working

--- nputil.py
+++ nputil.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def interleave(a, b):
     """Interleaves two one-dimensional arrays assuming the same length."""
     # https://stackoverflow.com/questions/5347065/interweaving-two-numpy-arrays
@@ -8,7 +9,17 @@ def interleave(a, b):
     c[1::2] = b
     return c
 
+
+def list_of_tuples_to_tuple_of_lists(list_of_tuples):
+    # https://stackoverflow.com/questions/7558908/unpacking-a-list-tuple-of-pairs-into-two-lists-tuples
+    return tuple(zip(*list_of_tuples))
+
+
 if __name__ == "__main__":
     a = np.array([1, 3, 5])
     b = np.array([2, 4, 6])
     print(interleave(a, b))
+
+    list_of_tuples = [(1, 2), (3, 4), (5, 6), (7, 8)]
+    print(f'{list_of_tuples=}')
+    print(f'{list_of_tuples_to_tuple_of_lists(list_of_tuples)=}')
